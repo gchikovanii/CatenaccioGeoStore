a8bc9ab8628c15a9805edc3422680a0db724736e
gchikovnaii25 <gchikovanii25@gmail.com>
2024-03-28 14:49:12 +0400

    structure changed and about us added

--- CatenaccioStore/client/src/app/home/home.component.ts
+++ CatenaccioStore/client/src/app/home/home.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
+export class HomeComponent  {
+ 
 }
