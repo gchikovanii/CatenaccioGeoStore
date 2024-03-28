56d44764f21a2218e678d50494342d2a8a7c8e11
gchikovnaii25 <gchikovanii25@gmail.com>
2024-03-28 10:15:42 +0400

    Navbar responsive

--- CatenaccioStore/CatenaccioStore.API/obj/Debug/net8.0/CatenaccioStore.API.AssemblyInfo.cs
+++ CatenaccioStore/CatenaccioStore.API/obj/Debug/net8.0/CatenaccioStore.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatenaccioStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41578e043ed996ea51331eda9d24555abe348bfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatenaccioStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatenaccioStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CatenaccioStore/client/src/app/nav-bar/nav-bar.component.ts
+++ CatenaccioStore/client/src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faHistory, faShoppingCart, faSignOut } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faBurger, faHistory, faShoppingCart, faSignOut, faUser, faUserCircle, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { BasketService } from '../services/basket.service';
 import { BaksetItem } from '../models/BasketItem';
 import { AccountService } from '../services/account.service';
@@ -15,6 +15,10 @@ export class NavBarComponent{
   cart = faShoppingCart;
   signout = faSignOut;
   history = faHistory;
+  account = faUserCircle;
+  loginUser = faUser;
+  registerUser = faUserPlus;
+  burger = faBars;
 
   getCount(items: BaksetItem[]){
     return items.reduce((sum,item) => sum + item.quantity,0);
