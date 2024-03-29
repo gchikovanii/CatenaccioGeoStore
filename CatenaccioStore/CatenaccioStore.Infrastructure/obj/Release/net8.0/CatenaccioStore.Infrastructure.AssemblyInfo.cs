095dff21875b79078ca5cdae0fb848832541b274
gchikovnaii25 <gchikovanii25@gmail.com>
2024-03-29 19:40:05 +0400

    s

--- CatenaccioStore/CatenaccioStore.Infrastructure/obj/Release/net8.0/CatenaccioStore.Infrastructure.AssemblyInfo.cs
+++ CatenaccioStore/CatenaccioStore.Infrastructure/obj/Release/net8.0/CatenaccioStore.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatenaccioStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+284241a960a05060947d26c91246af500fbfbe67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b15fe289ceaf033baff26ce3b434022c56d81fb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatenaccioStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatenaccioStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
